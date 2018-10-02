1a5d0277a69871792b027dd735d878bb4404f5af
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-02 14:44:02 -0400

    Updated to include moving piece down with down arrow

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const matrix = [
     [0, 1, 0],
 ]
 
+function createMatrix(w, h) {
+    const matrix = [];
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
 function draw() {
     context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
@@ -26,6 +34,10 @@ function drawMatrix(matrix, offset){
     })
 };
 
+function playerDrop() {
+    player.pos.y++;
+    dropCounter = 0;
+}
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -35,12 +47,13 @@ function update(time = 0) {
     lastTime = time;
     dropCounter += deltaTime;
     if(dropCounter > dropInterval) {
-        player.pos.y++;
-        dropCounter = 0;
+        playerDrop();
     }
     draw();
     requestAnimationFrame(update);
 }
+
+const arena = createMatrix(12, 20);
 
 const player = {
     pos: {x: 5, y: 5},
@@ -52,7 +65,9 @@ document.addEventListener('keydown', event => {
         player.pos.x--;
     } else if(event.keyCode === 39) {
         player.pos.x++;
+    } else if(event.keyCode === 40) {
+        playerDrop();
     }
-})
+});
 
 update();
