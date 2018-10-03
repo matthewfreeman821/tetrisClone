9bc32944107d48855e0ff470a23d67d33800fb1a
Matthew Freeman <matthew.freeman821@gmail.com>
2018-10-03 12:55:04 -0400

    Added function to clear a row when it is filled

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
+function arenaSweep() {
+    outer: for (let y = arena.length - 1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+    }
+}
+
 function collide(arena, player) {
     const [m, o] = [player.matrix, player.pos];
     for (let y = 0; y < m.length; ++y) {
@@ -32,38 +45,38 @@ function createPiece(type) {
         ];
     } else if (type === 'O') {
         return [
-            [1, 1],
-            [1, 1],
+            [2, 2],
+            [2, 2],
         ];
     } else if (type === 'L') {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1],
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
         ];
     } else if (type === 'J') {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0],
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
         ];
     } else if (type === 'I') {
         return [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
         ];
     } else if (type === 'S') {
         return [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0],
         ];
     } else if (type === 'Z') {
         return [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0],
         ];
     }
@@ -80,7 +93,7 @@ function drawMatrix(matrix, offset){
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if(value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         })
@@ -103,6 +116,7 @@ function playerDrop() {
         player.pos.y--;
         merge(arena, player);
         playerReset();
+        arenaSweep();
     }
     dropCounter = 0;
 }
@@ -172,6 +186,17 @@ function update(time = 0) {
     draw();
     requestAnimationFrame(update);
 }
+
+const colors = [
+    null,
+    '#FF0D72',
+    '#0DC2FF',
+    '#0DFF72',
+    '#F538FF',
+    '#FF8E0D',
+    '#FFE138',
+    '#3877FF',
+];
 
 const arena = createMatrix(12, 20);
 
